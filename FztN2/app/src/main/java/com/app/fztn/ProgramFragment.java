ac095ebe98089ee930cd2fd6d8be3e6c9c75d478
mlsgcl <f201213076@ktun.edu.tr>
2024-06-11 16:34:26 +0300

    commit

--- FztN2/app/src/main/java/com/app/fztn/ProgramFragment.java
+++ FztN2/app/src/main/java/com/app/fztn/ProgramFragment.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class ProgramFragment extends Fragment {
     private DbAdapter dbAdapter;
@@ -38,21 +44,25 @@ public class ProgramFragment extends Fragment {
 
 
 
-        Button buttonVideo = view.findViewById(R.id.Video);
-        buttonVideo.setOnClickListener(new View.OnClickListener() {
+        LinearLayout linearLayout1 = view.findViewById(R.id.linearlayout1);
+        WebView webViewVideoPlayer = view.findViewById(R.id.webViewVideoPlayer);
+
+        // JavaScript'i etkinleştir
+        WebSettings webSettings = webViewVideoPlayer.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // WebViewClient ve WebChromeClient ayarla
+        webViewVideoPlayer.setWebViewClient(new WebViewClient());
+        webViewVideoPlayer.setWebChromeClient(new WebChromeClient());
+
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Kullanıcı ID'sini burada alabilirsiniz
                 String recommendedVideoUrl = dbAdapter.getRecommendedVideoUrl(userId);
-
-                // Başlık ve URL'yi göstermek için gerekli bileşenleri bulun
-                WebView webViewVideoPlayer = view.findViewById(R.id.webViewVideoPlayer);
-
-
+                // WebView'i görünür yap ve URL'yi yükle
+                webViewVideoPlayer.setVisibility(View.VISIBLE);
                 webViewVideoPlayer.loadUrl(recommendedVideoUrl);
-
-
-
             }
         });
 
